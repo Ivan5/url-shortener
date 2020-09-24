dc92a0e7e583cc945fa0e761f71bcab7466e72a0
Ivan5 <converse_ivan@hotmail.com>
2020-09-24 13:43:43 -0500

    setting up mongoose and mongoose schema

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+const ShortUrl = require("./models/url");
 
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("index", { myVariable: "This is a test" });
+  res.render("index");
 });
 
-app.listen(5000, function () {
-  console.log("Server started");
+app.post("/short", async function (req, res) {
+  const record = new ShortUrl({
+    full: "test",
+  });
+
+  await record.save();
+  res.json({ ok: 1 });
+});
+
+mongoose.connect("mongodb://localhost:27017/short", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("open", function () {
+  app.listen(5000, function () {
+    console.log("Server started");
+  });
 });
