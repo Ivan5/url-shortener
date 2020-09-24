a18e36eb6ae03f25de4ab5fac48a9a0be1dae229
Ivan5 <converse_ivan@hotmail.com>
2020-09-24 12:46:09 -0500

    setting up view engine

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-  res.send("Hello world");
+  res.render("index", { myVariable: "This is a test" });
 });
 
 app.listen(5000, function () {
